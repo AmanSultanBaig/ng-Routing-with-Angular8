2d84b347bfd6b2ad61d1eb5c4b7c19826ad6eb92
Aman Sultan Baig <mac@macs-MacBook-Pro.local>
2019-12-02 15:53:04 +0500

    routing configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { DepartmentDetailsComponent } from './components/department-details/department-details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'departments',component:DepartmentsComponent},
+  {path:'department-details',component:DepartmentDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+export const MyComponents = [
+  DepartmentsComponent,
+  DepartmentDetailsComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, MyComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponents
   ],
   imports: [
     BrowserModule,
